5c303ff7ef8feb197e793134f05a660d25897736
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2017-09-21 13:22:13 -0700

    Improved dungeon generation
    Put the pathmaking code into a function and fixed the troom generation

--- dungen.rb
+++ dungen.rb
@@ -17,6 +17,8 @@ class Room
 			print("@")
 		when 3 # boss room
 			print("!")
+		when 4 # treasure room
+			print("$")
 		else # empty space
 			print(" ")
 		end
@@ -24,6 +26,53 @@ class Room
 end
 
 class Dungeon
+	
+	def createPath(rng, m, start, finish, y1, dy1)
+		y = y1
+		dy = dy1
+		for x in start[0]..finish[0]
+			if x == finish[0]
+				# go to the treasure room regardless of number of remaining rooms
+				if dy >= 0
+					for i in y..finish[1]
+						@map[i][x].setType(1)
+					end
+				elsif dy < 0
+					for i in finish[1]..y
+						@map[i][x].setType(1)
+					end
+				end
+			else
+				if dy >= 0
+					targety = y + rng.rand(0..2)
+					if targety > @h - 1
+						targety = @h - 1
+					end
+					
+					for i in y..targety
+						@map[i][x].setType(1)
+					end
+					
+					# update new y position
+					y = targety
+					dy = (finish[1] - y)
+				elsif dy < 0
+					targety = y - rng.rand(0..2)
+					if targety < 0
+						targety = 0
+					end
+					
+					for i in targety..y
+						@map[i][x].setType(1)
+					end
+					
+					# update new y position
+					y = targety
+					dy = (finish[1] - y)
+				end
+			end
+		end
+	end
 	
 	def setSize(w, h)
 		# array of rooms
@@ -48,62 +97,40 @@ class Dungeon
 		boss = [ rng.rand((@w / 2)..(@w - 1)), rng.rand(0..(@h - 1)) ]
 		
 		# get manhattan distance (in rooms)
-		taxi_distance = (spawn[0] - boss[0]).abs + (spawn[1] - boss[1]).abs
+		#taxi_distance = (spawn[0] - boss[0]).abs + (spawn[1] - boss[1]).abs
 		
 		# get a target number of rooms between spawn and boss
-		extra_rooms = rng.rand(0..((@w / 2) - 1)) * 2 # needs to be even
-		target_rooms = taxi_distance + extra_rooms
+		#extra_rooms = rng.rand(0..((@w / 2) - 1)) * 2 # needs to be even
+		#target_rooms = taxi_distance + extra_rooms
 		
 		# make a path
-		rooms = 0
+		#rooms = 0
 		y = spawn[1]
 		dy = (boss[1] - spawn[1] <=> 0) # this is -1 if the number is negative, 0 if it's zero, or 1 if it's positive
-		for x in spawn[0]..boss[0]
-			if x == boss[0]
-				# go to the boss room regardless of number of remaining rooms
-				if dy >= 0
-					for i in y..boss[1]
-						@map[i][x].setType(1)
-					end
-				elsif dy < 0
-					for i in boss[1]..y
-						@map[i][x].setType(1)
-					end
-				end
+
+		createPath(rng, @map, spawn, boss, y, dy)
+		# add up to 4 treasure rooms
+		trooms = Array.new(rng.rand(1..3)) { Array.new(2) { 0 } }
+		for t in 0..trooms.size
+			trooms[t] = [ rng.rand(0..(@w - 1)), rng.rand(0..(@h - 1)) ]
+			#y = spawn[1]
+			dx = (trooms[t][0] - spawn[0] <=> 0)
+			dy = (trooms[t][1] - spawn[1] <=> 0)
+			
+			# find a path
+			if (trooms[t][0] >= spawn[0])
+				createPath(rng, @map, spawn, trooms[t], y, dy)
 			else
-				if dy >= 0
-					targety = y + rng.rand(0..2)
-					if targety > @h - 1
-						targety = @h - 1
-					end
-					
-					for i in y..targety
-						@map[i][x].setType(1)
-					end
-					
-					# update new y position
-					y = targety
-					dy = (boss[1] - y)
-				elsif dy < 0
-					targety = y - rng.rand(0..2)
-					if targety < 0
-						targety = 0
-					end
-					
-					for i in targety..y
-						@map[i][x].setType(1)
-					end
-					
-					# update new y position
-					y = targety
-					dy = (boss[1] - y)
-				end
+				y = trooms[t][1]
+				createPath(rng, @map, trooms[t], spawn, y, dy)
 			end
 		end
 		
-		# add a treasure room
-		troom = [ rng.rand(0..(@w - 1)), rng.rand(0..(@h - 1)) ]
 		
+		# set trooms
+		for r in trooms
+			@map[r[1]][r[0]].setType(4)
+		end
 		
 		# set spawn room
 		@map[spawn[1]][spawn[0]].setType(2)
